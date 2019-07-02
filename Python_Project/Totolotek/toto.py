f7d5ca01ba66eb8fe877ea6320d2783bdb015cc1
Adrian Kruczak <adriankruczak@gmail.com>
2019-07-02 22:57:16 +0200

    Improve mechanism of totolotek

--- Python_Project/Totolotek/toto.py
+++ Python_Project/Totolotek/toto.py
@@ -3,6 +3,16 @@
 
 import random
 
+LICZBA_PROB = 5
 liczba = random.randint(1, 10)
 
-print("Wylosowana liczba:",liczba)
+
+
+for proba in range(0, LICZBA_PROB):
+    
+    answer = input("Jaką liczbę od 1 do 10 mam na myśli? ")
+
+    if liczba == int(answer):
+        print("Zgadłeś! Dostajesz dlugopis!")
+        break
+    else : print(f'Niestety wylosowana liczba to: {liczba}')
