ae780dae547a267b3d974ff76e6736c8dd2cc531
Adrian <adriankruczak@gmail.com>
2019-12-02 21:55:36 +0100

    warm up

--- Python_Project/CodeWars/level_8.py
+++ Python_Project/CodeWars/level_8.py
@@ -1054,7 +1054,9 @@ def how_many_dalmatians(n):
     dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"]
   
     respond = dogs[0] if n <= 10 else dogs[1] if n <= 50  else dogs[3] if n ==101 else dogs[2]
-    
+
     return respond
 
-print(how_many_dalmatians(101))
+#Grasshopper - Terminal game move function
+def move(position, roll):
+    return position + 2*roll
