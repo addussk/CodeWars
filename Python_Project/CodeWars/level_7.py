e272f5582f04d9c6959be798943245c556d65ff1
addussk <34662199+addussk@users.noreply.github.com>
2020-12-30 18:14:45 +0100

    11 solutions on 7lvl, 1390 left

--- Python_Project/CodeWars/level_7.py
+++ Python_Project/CodeWars/level_7.py
@@ -99,3 +99,75 @@ def find_next_square(sq):
     # Return the next square if sq is a square, -1 otherwise
     return -1 if not ((sq**(1/2)//1)**2 == sq) else (sq**(1/2) + 1)**2
 
+# Two to One
+def longest(s1, s2):
+    seen = set()
+    return "".join( let for let in sorted(s1 + s2) if let not in seen and not seen.add(let) )
+
+# Regex validate PIN code
+def validate_pin(pin):
+    return len(pin) in (4, 6) and pin.isdigit()
+
+# Sum of odd numbers
+def row_sum_odd_numbers(n):
+    firstNum, x, retSum = 0, 2, []
+    numElem = sum(range(n+1))
+
+    if firstNum == 0:
+        firstNum += 1
+        retSum.append(1)
+
+    while(firstNum < numElem):
+        x += 1
+
+        if x%2:
+            firstNum += 1
+            retSum.append(x)
+
+    return sum(retSum[-n:])
+
+# Binary Addition
+def add_binary(a,b):
+    return bin(a+b)[2:]
+
+# Friend or Foe?
+def friend(x):
+    return [ name for name in x if len(name) == 4 ]
+
+# Categorize New Member
+def open_or_senior(data):
+    return [ "Senior" if (member[0] >= 55 and member[1] > 7) else "Open" for member in data ]
+
+# Ones and Zeros
+def binary_array_to_number(arr):
+    return int(("0b" + "".join(str(el) for el in arr)), 2)
+
+# Find the divisors!
+def divisors(integer):
+    divisors = []
+    for i in range(2,integer):
+        if integer % i == 0:
+            divisors.append(i)
+            
+    return divisors if divisors else "{} is prime".format(integer)
+
+# Number of People in the Bus
+def number(bus_stops):
+    return sum([getIn[0] for getIn in bus_stops]) - sum([ getOff[1] for getOff in bus_stops ])
+
+# String ends with?
+def solution(string, ending):
+    return ( string[:string.rfind(ending)] + ending ) == string
+
+# Sum of the first nth term of Series
+def series_sum(n):
+    retArr = []
+    if n == 0:
+        return "0.00"
+    if n < 2 :
+        return "1.00"
+    else:
+        for elm in range(1,n*3,3):
+            retArr.append((1 / elm))
+        retVal = str(round(sum( retArr ),2 ))
+        return retVal if len(retVal) == 4 else retVal + "0"
